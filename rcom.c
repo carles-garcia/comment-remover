c6463ea3098302809d7ee1313a69268af0230508
carles-garcia <>
2015-09-24 15:34:15 +0200

    white detection seems to work

--- rcom.c
+++ rcom.c
@@ -4,12 +4,12 @@
 //it might not work with lines longer than 1023 characters 
 void rcom(FILE *source, FILE *output, int lang, struct options *opts) {
   char buffer[BUFSIZE], copy[BUFSIZE];
-  char quote = 0;
-  int comment = 0, i, finish, white;
+  char quote;
+  int comment = 0, i, finish, white, has_com;
   
   // TODO doxy
   while (fgets(buffer, sizeof buffer, source)) {
-    for (i = 0, finish = 0, white = 1; !finish; ++i) { 
+    for (i = 0, finish = 0, white = 1, has_com = comment, quote = 0; !finish; ++i) { 
       /* Warning: this doesn't work if last line doesn't end wih \n,
        * however according to POSIX all text files you end with it.
        * Tools like cat & grep don't work without \n in the last line neither.
@@ -24,11 +24,15 @@ void rcom(FILE *source, FILE *output, int lang, struct options *opts) {
 	case '/' :
 	  if (quote) copy[i] = buffer[i];
 	  else if (!comment) {
-	    if (opts->inlin && buffer[i+1] == '/') finish = 1;
+	    if (opts->inlin && buffer[i+1] == '/') {
+	      finish = 1;
+	      has_com = 1;
+	    }
 	    else if (buffer[i+1] == '*') {
 	      if (opts->jdoc && buffer[i+2] == '*') {
 		comment = 1;
 		i += 2;
+		has_com = 1;
 	      }
 	      else if (opts->block) {
 		if (buffer[i+2] == '*') {
@@ -42,6 +46,7 @@ void rcom(FILE *source, FILE *output, int lang, struct options *opts) {
 		else {
 		  comment = 1;
 		  ++i;
+		  has_com = 1;
 		}
 	      }
 	    }
@@ -75,7 +80,7 @@ void rcom(FILE *source, FILE *output, int lang, struct options *opts) {
 	// This case is for characters like \" 
 	case '\\' : // review escape sequences... is it necessary
 	  if (!comment) {
-	    white = 0;
+	    //white = 0;
 	    copy[i] = buffer[i];
 	    ++i;
 	    copy[i] = buffer[i];
@@ -89,14 +94,12 @@ void rcom(FILE *source, FILE *output, int lang, struct options *opts) {
 	  }
       }
     }
-    if (white) {
-      copy[0] = '\n';
-      copy[1] = '\0';
-    }
-    else {
+    if (white && !has_com && !comment) white = 0;
+    if (!white) {
       copy[i-1] = '\n';
       copy[i] = '\0';
+      fputs(copy, output);
     }
-    fputs(copy, output); 
+     
   }
 }
